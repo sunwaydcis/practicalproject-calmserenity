28be8d92b5ddaa00f42b04eba2cb55e3bd9fa7fd
Serene <serenengky04@gmail.com>
2025-07-28 02:49:43 +0800

    practical 2 and 3

--- src/main/scala-3/ch/makery/address/MainApp.scala
+++ src/main/scala-3/ch/makery/address/MainApp.scala
@@ -1,17 +1,21 @@
 package ch.makery.address
 
+import ch.makery.address.model.Person
 import javafx.fxml.FXMLLoader
 import scalafx.application.JFXApp3
 import scalafx.application.JFXApp3.PrimaryStage
 import scalafx.scene.Scene
 import scalafx.Includes.*
 import javafx.scene as jfxs
+import scalafx.collections.ObservableBuffer
 
 object MainApp extends JFXApp3: 
   
   //window root pane 
-  var roots: Option[scalafx.scene.layout.BorderPane] = None 
-  
+  var roots: Option[scalafx.scene.layout.BorderPane] = None
+
+  val personData = new ObservableBuffer[Person]()
+
   override def start(): Unit =
     val rootResource = getClass.getResource("view/RootLayout.fxml")
     val loader= new FXMLLoader(rootResource)
@@ -24,14 +28,27 @@ object MainApp extends JFXApp3:
       scene = new Scene(): 
         root = roots.get 
         showPersonOverview()
+
     def showPersonOverview(): Unit =
       val resource = getClass.getResource("view/PersonOverview.fxml")
       val loader = new FXMLLoader(resource)
       loader.load()
       val roots = loader.getRoot[jfxs.layout.AnchorPane]
-      this.roots.get.center = roots 
-      
-        
+      this.roots.get.center = roots
+
+
+    personData += new Person("Hans", "Muster")
+    personData += new Person("Ruth", "Mueller")
+    personData += new Person("Heinz", "Kurz")
+    personData += new Person("Cornelia", "Meier")
+    personData += new Person("Werner", "Meyer")
+    personData += new Person("Lydia", "Kunz")
+    personData += new Person("Anna", "Best")
+    personData += new Person("Stefan", "Meier")
+    personData += new Person("Martin", "Mueller")
+
+
+
         
 
 
